e1c91a5f4ea0eb4f3b4bf88bd0416a69e7cfa12f
Marvin Brereton <104062149+marvin-brereton@users.noreply.github.com>
2022-05-17 09:52:04 +0100

    Created base case template

---
+++ CultureDemo/Controllers/CaseController.cs
@@ -0,0 +1,49 @@
+﻿using CultureDemo.Models.Case;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CultureDemo.Controllers
+{
+    public class CaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var caseDetails = new CaseDTO();
+
+            var casePresenter = new CasePresenter();
+
+            caseDetails.CaseList = casePresenter.GeneratedCaseList()
+                .OrderBy(e => e.Case_CreatedDateT)
+                .ToList();
+
+            return View(caseDetails);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCase()
+        {
+            return View(new CaseDTO());
+        }
+
+        [HttpPost]
+        public IActionResult CreateCase(CaseBO caseDetails)
+        {
+            return View(caseDetails);
+        }
+
+        public IActionResult Details(string cName, string cDesc, string cStatus, string cDateT, string mDateT, string cBy, string mBy)
+        {
+            var caseBO = new CaseBO
+            {
+                Case_Name = cName,
+                Case_Description = cDesc,
+                Case_Status = cStatus,
+                Case_CreatedDateT = cDateT,
+                Case_ModifiedDateT = mDateT,
+                User_CreatedBy = cBy,
+                User_ModifiedBy = mBy
+            };
+
+            return View(caseBO);
+        }
+    }
+}
---
+++ CultureDemo/Models/User/UserBO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CultureDemo.Models.User
+{
+    public class UserBO
+    {
+        [Required(ErrorMessage = "Your first name is required")]
+        [Display(Name = "FirstName")]
+        public string? User_FirstName { get; set; }
+
+        [Display(Name = "SecondName")]
+        [Required(ErrorMessage = "Your second name is required")]
+        public string? User_LastName { get; set; }
+
+        [Display(Name = "CaseCreatedBy")]
+        public string User_CreatedBy { get; set; }
+
+        [Display(Name = "CaseModifiedBy")]
+        public string? User_ModifiedBy { get; set; }
+    }
+}
